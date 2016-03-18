1ead1bdd43c234159607fd96e1fcc947ea4afb6e
Tim Ransom <ransomts@appstate.edu>
2016-03-18 11:37:35 -0400

    Added more Haskell
    as well as removed extranious documentation, added a main method, and
    type signatures

--- lists.hs
+++ lists.hs
@@ -1,76 +1,37 @@
+main :: IO()
+main = do
+  print (flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]))
+
+
 -- 1 Problem 1
 -- (*) Find the last element of a list.
-
--- (Note that the Lisp transcription of this problem is incorrect.)
-
--- Example in Haskell:
-
--- Prelude> myLast [1,2,3,4]
--- 4
--- Prelude> myLast ['x','y','z']
--- 'z'
-
+myLast :: [c] -> c
 myLast = head . reverse
 
 -- 2 Problem 2
 -- (*) Find the last but one element of a list.
-
--- (Note that the Lisp transcription of this problem is incorrect.)
-
--- Example in Haskell:
-
--- Prelude> myButLast [1,2,3,4]
--- 3
--- Prelude> myButLast ['a'..'z']
--- 'y'
-
+myButLast :: [c] -> c
 myButLast = head . tail . reverse
 
 -- 3 Problem 3
 -- (*) Find the K'th element of a list. The first element in the list is number 1.
-
--- Example:
-
--- * (element-at '(a b c d e) 3)
--- c
--- Example in Haskell:
-
--- Prelude> elementAt [1,2,3] 2
--- 2
--- Prelude> elementAt "haskell" 5
--- 'e'
-
 --elementAt xs n = xs !! (n - 1)
 elementAt xs 1 = head xs
 elementAt xs n = elementAt (tail xs) (n - 1)
 
 -- 4 Problem 4
 -- (*) Find the number of elements of a list.
-
--- Example in Haskell:
-
--- Prelude> myLength [123, 456, 789]
--- 3
--- Prelude> myLength "Hello, world!"
--- 13
-
+myLength :: Num a => [t] -> a
 myLength [] = 0
 myLength xs = (myLength (tail xs)) + 1
 
+-- 5 Problem 5
+-- (*) Reverse a list.
 myReverse [] = []
 myReverse xs =   (myReverse (tail xs)) ++ [head xs]
 
 -- 6 Problem 6
 -- (*) Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
-
--- Example in Haskell:
-
--- *Main> isPalindrome [1,2,3]
--- False
--- *Main> isPalindrome "madamimadam"
--- True
--- *Main> isPalindrome [1,2,4,8,16,8,4,2,1]
--- True
 isPalindrome [] = True
 isPalindrome [a] = True
 isPalindrome xs
@@ -81,9 +42,10 @@ isPalindrome xs
 -- (**) Flatten a nested list structure.
 
 -- Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
-
--- Example:
-
--- * (my-flatten '(a (b (c d) e)))
--- (A B C D E)
-flatten [] = []
+data NestedList a = Elem a | List [NestedList a]
+flatten :: NestedList a -> [a]
+flatten = reverse . rec []
+  where
+  rec acc (List []) = acc
+  rec acc (Elem x)  = x:acc
+  rec acc (List (x:xs)) = rec (rec acc x) (List xs)
