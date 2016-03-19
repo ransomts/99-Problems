039dd7fa23c326d7db4ceba045ec7c24f7d6ae26
Tim Ransom <ransomts@appstate.edu>
2016-03-19 00:35:21 -0400

    problem 10 solution

--- lists.hs
+++ lists.hs
@@ -70,3 +70,5 @@ pack (x : xs) = if x `elem` (head (pack xs))
 
 -- 10 Problem 10
 -- (*) Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+encode :: Eq t => [t] -> [(Int, t)]
+encode xs = map (\x -> (length x, head x)) (pack xs)
